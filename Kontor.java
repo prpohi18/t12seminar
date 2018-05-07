1ffba9f50964cec56f6adac1b4686292c229381d
Julika Maiste <julimai@users.noreply.github.com>
2018-05-07 08:55:43 +0300

    seminar

---
+++ Kontor.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class Kontor{
+	Map<String, Konto> kontod=new HashMap<String, Konto>();
+	public Kontor(){
+		loeKontod();
+	}
+	protected void loeKontod(){
+		String[] fnimed=new File(Konto.kontokaust).list();
+		for(String fnimi: fnimed){
+			String[] m=fnimi.split("\\.");
+			if(m.length==2){
+				kontod.put(m[0], new Konto(m[0]));
+				System.out.println("Loodi: "+m[0]);
+			}
+		}		
+	}
+	public String kontoSisu(String kontonr){
+		if(kontod.get(kontonr)!=null){
+			return"Konto "+kontonr+" saldo "+kontod.get(kontonr).loeSumma();
+		}
+		return "Konto "+kontonr+" puudub";
+	}
+	//Käsklus raha üle kandmiseks ühelt kontolt teisele.
+	public boolean ylekanne (String kellelt, String kellele, int summa){
+		if(kontod.get(kellelt)==null){return false;}
+		if(kontod.get(kellele)==null){return false;}
+		if(kontod.get(kellelt).loeSumma()<summa){return false;}
+		kontod.get(kellelt).eemaldaSumma(summa);
+		kontod.get(kellele).lisaSumma(summa);
+		return true;
+	}
+	
+}
